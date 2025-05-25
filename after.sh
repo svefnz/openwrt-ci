93f326abd6e4f4fa91cbce61835a66e4fc9db4e1
朝代尾 <52125806+svefnz@users.noreply.github.com>
2025-05-26 02:14:42 +0800

    Update after.sh

--- after.sh
+++ after.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Modify default IP
+sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/luci2/bin/config_generate
 sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
@@ -8,3 +9,9 @@ sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generat
 
 # Modify hostname
 sed -i 's/OpenWrt/OneCloud/g' package/base-files/files/bin/config_generate
+
+# 修改默认主机名
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='OneCloud'' package/lean/default-settings/files/zzz-default-settings
+
+# 加入编译者信息
+sed -i "s/OpenWrt /svefnz build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
